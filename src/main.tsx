76b7888c2418fc5377e293e2c8ac92f4aaf1171f
brother1-4752 <guddlf4752@gmail.com>
2023-12-22 01:32:18 +0900

    chore: reocil 패키지 설치 / 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/Router.tsx";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme.ts";
 import { GlobalStyle } from "./styles/GlobalStyle.tsx";
+import { RecoilRoot } from "recoil";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <RecoilRoot>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <RouterProvider router={router} />
     </ThemeProvider>
-  </React.StrictMode>
+  </RecoilRoot>
+  // </React.StrictMode>
 );
